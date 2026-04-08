64b4d761ffd53316a0509a20d2ce21780888ebe1
Retr0 <redac618@gmail.com>
2026-04-08 14:49:36 +0100

    Add hello.c - first C program

---
+++ hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+printf("Hello, Git \& GitHub Workshop!\n");
+printf("This file is now tracked with Git!\n");
+printf("Compiled on: February 17, 2026\n");
+return 0;
+}
