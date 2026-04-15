a60768d4e9c56c513950a7cc7a73c327ef21e951
Retr0 <redac618@gmail.com>
2026-04-15 14:54:07 +0100

    Version 15/4/2025

--- hello.c
+++ hello.c
@@ -3,5 +3,6 @@ int main() {
 printf("Hello, Git \& GitHub Workshop!\n");
 printf("This file is now tracked with Git!\n");
 printf("This is a practical git course\n");
+printf("I just landed a git file\n");
 return 0;
 }
